8dacc75455f2332d01be081cb75c111b6c2ef22f
Eugene <baskin.robinz@gmail.com>
2018-05-02 22:44:16 +0300

    fallbacks and fixed bugs for ie11

--- app/js/main.js
+++ app/js/main.js
@@ -1,40 +1,83 @@
 
 const cards = document.querySelectorAll('.card--wrapper');
 
-cards.forEach(card => {
-	let cardItem = card.querySelector('.card__outer');
-	let offerDefault = card.querySelector('.card__offer_default');
-	let offerDisabled = card.querySelector('.card__offer_disabled');
+const miracleOne = function () {
+	for (let i = 0; i < cards.length; i++) {
+		let cardItem = cards[i].querySelector('.card__outer');
+		let offerDefault = cards[i].querySelector('.card__offer_default');
+		let offerDisabled = cards[i].querySelector('.card__offer_disabled');
 
-	if (cardItem.classList.contains('deactive')) {
-		offerDefault.style.display = 'none';
-		offerDisabled.style.display = 'block';
-	}
-});
-
-cards.forEach(card => card.addEventListener('click', function() {
-	let cardItem = card.querySelector('.card__outer');
-	let offerDefault = card.querySelector('.card__offer_default');
-	let offerSelected = card.querySelector('.card__offer_selected');
-	let offeDisabled = card.querySelector('.card__offer_disabled');
-
-	if (!cardItem.classList.contains('deactive')) {
-		if (!cardItem.classList.contains('active')) {
-		cardItem.classList.add('active', 'no-hover');		
-		offerDefault.style.display = 'none';
-		offerSelected.style.display = 'block';
-		cardItem.addEventListener('mouseout', function() {
-			cardItem.classList.remove('no-hover');
-		});
-		} else {
-			cardItem.classList.remove('active');
-			offerSelected.style.display = 'none';
-			offerDefault.style.display = 'block';		
+		if (cardItem.classList.contains('deactive')) {
+			offerDefault.style.display = 'none';
+			offerDisabled.style.display = 'block';
 		};
-	} else {
-		return false;
 	}
-}));
+};
+miracleOne();
+
+const miracleTwo = function() {
+	for (let i = 0; i < cards.length; i++) {
+		cards[i].addEventListener('click', function() {
+			let cardItem = cards[i].querySelector('.card__outer');
+			let offerDefault = cards[i].querySelector('.card__offer_default');
+			let offerSelected = cards[i].querySelector('.card__offer_selected');
+
+			if (!cardItem.classList.contains('deactive')) {
+				if (!cardItem.classList.contains('active')) {
+				cardItem.classList.add('active', 'no-hover');		
+				offerDefault.style.display = 'none';
+				offerSelected.style.display = 'block';
+				cardItem.addEventListener('mouseout', function() {
+					cardItem.classList.remove('no-hover');
+				});
+				} else {
+					cardItem.classList.remove('active');
+					offerSelected.style.display = 'none';
+					offerDefault.style.display = 'block';		
+				}
+			} else {
+				return false;
+			}
+		});
+	}
+}
+miracleTwo();
+
+// * For all browsers BESIDES ie11! =))
+
+// cards.forEach(card => {
+// 	let cardItem = card.querySelector('.card__outer');
+// 	let offerDefault = card.querySelector('.card__offer_default');
+// 	let offerDisabled = card.querySelector('.card__offer_disabled');
+
+// 	if (cardItem.classList.contains('deactive')) {
+// 		offerDefault.style.display = 'none';
+// 		offerDisabled.style.display = 'block';
+// 	}
+// });
+
+// cards.forEach(card => card.addEventListener('click', function() {
+// 	let cardItem = card.querySelector('.card__outer');
+// 	let offerDefault = card.querySelector('.card__offer_default');
+// 	let offerSelected = card.querySelector('.card__offer_selected');
+
+// 	if (!cardItem.classList.contains('deactive')) {
+// 		if (!cardItem.classList.contains('active')) {
+// 		cardItem.classList.add('active', 'no-hover');		
+// 		offerDefault.style.display = 'none';
+// 		offerSelected.style.display = 'block';
+// 		cardItem.addEventListener('mouseout', function() {
+// 			cardItem.classList.remove('no-hover');
+// 		});
+// 		} else {
+// 			cardItem.classList.remove('active');
+// 			offerSelected.style.display = 'none';
+// 			offerDefault.style.display = 'block';		
+// 		};
+// 	} else {
+// 		return false;
+// 	}
+// }));
 
 
 
---
+++ main.js
@@ -0,0 +1,45 @@
+
+const cards = document.querySelectorAll('.card--wrapper');
+
+
+
+
+// * For all browsers BESIDES ie11! =))
+
+// cards.forEach(card => {
+// 	let cardItem = card.querySelector('.card__outer');
+// 	let offerDefault = card.querySelector('.card__offer_default');
+// 	let offerDisabled = card.querySelector('.card__offer_disabled');
+
+// 	if (cardItem.classList.contains('deactive')) {
+// 		offerDefault.style.display = 'none';
+// 		offerDisabled.style.display = 'block';
+// 	}
+// });
+
+// cards.forEach(card => card.addEventListener('click', function() {
+// 	let cardItem = card.querySelector('.card__outer');
+// 	let offerDefault = card.querySelector('.card__offer_default');
+// 	let offerSelected = card.querySelector('.card__offer_selected');
+
+// 	if (!cardItem.classList.contains('deactive')) {
+// 		if (!cardItem.classList.contains('active')) {
+// 		cardItem.classList.add('active', 'no-hover');		
+// 		offerDefault.style.display = 'none';
+// 		offerSelected.style.display = 'block';
+// 		cardItem.addEventListener('mouseout', function() {
+// 			cardItem.classList.remove('no-hover');
+// 		});
+// 		} else {
+// 			cardItem.classList.remove('active');
+// 			offerSelected.style.display = 'none';
+// 			offerDefault.style.display = 'block';		
+// 		};
+// 	} else {
+// 		return false;
+// 	}
+// }));
+
+
+
+	
