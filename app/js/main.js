0764e31ce1fdd998919ad41f93d343d2605af0f1
Eugene <baskin.robinz@gmail.com>
2018-05-14 00:01:19 +0300

    refactoring

--- app/js/main.js
+++ app/js/main.js
@@ -1,5 +1,5 @@
 
-const cards = document.querySelectorAll('.card--wrapper');
+const cards = document.querySelectorAll('.card');
 
 const miracleOne = function () {
 	for (let i = 0; i < cards.length; i++) {
@@ -17,23 +17,40 @@ miracleOne();
 
 const miracleTwo = function() {
 	for (let i = 0; i < cards.length; i++) {
-		cards[i].addEventListener('click', function() {
-			let cardItem = cards[i].querySelector('.card__outer');
-			let offerDefault = cards[i].querySelector('.card__offer_default');
-			let offerSelected = cards[i].querySelector('.card__offer_selected');
-
-			if (!cardItem.classList.contains('deactive')) {
-				if (!cardItem.classList.contains('active')) {
-				cardItem.classList.add('active', 'no-hover');		
-				offerDefault.style.display = 'none';
-				offerSelected.style.display = 'block';
-				cardItem.addEventListener('mouseout', function() {
-					cardItem.classList.remove('no-hover');
-				});
+		let cardItem = cards[i].querySelector('.card__outer');
+		let cardLink = cards[i].querySelector('.card__offer_link');
+		let offerDefault = cards[i].querySelector('.card__offer_default');
+		let offerSelected = cards[i].querySelector('.card__offer_selected');
+		
+		cardItem.addEventListener('click', function() {			
+			if (!this.classList.contains('deactive')) {
+				if (!this.classList.contains('active')) {
+					this.classList.add('active', 'no-hover');		
+					offerDefault.style.display = 'none';
+					offerSelected.style.display = 'block';					
+					this.addEventListener('mouseout', function() {
+						this.classList.remove('no-hover');
+					});
 				} else {
-					cardItem.classList.remove('active');
+					this.classList.remove('active');
 					offerSelected.style.display = 'none';
 					offerDefault.style.display = 'block';		
+				}
+			} else {
+				return false;
+			}
+		});
+
+		cardLink.addEventListener('click', function(e) {
+			e.preventDefault();			
+			if (!cardItem.classList.contains('deactive')) {
+				if (!cardItem.classList.contains('active')) {
+					cardItem.classList.add('active', 'no-hover');		
+					offerDefault.style.display = 'none';
+					offerSelected.style.display = 'block';
+					cardItem.addEventListener('mouseout', function() {
+						this.classList.remove('no-hover');
+					});
 				}
 			} else {
 				return false;
@@ -43,7 +60,11 @@ const miracleTwo = function() {
 }
 miracleTwo();
 
+
+
 // * For all browsers BESIDES ie11! =))
+
+// const cards = document.querySelectorAll('.card');
 
 // cards.forEach(card => {
 // 	let cardItem = card.querySelector('.card__outer');
